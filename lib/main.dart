3ef3d206c1056bbad281c181db282d933078319c
kayuyu.mwaura <kayuyu.mwaura@tovutigroup.com>
2020-07-14 02:54:06 +0300

    Add reset button

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,12 @@ class FuelForm extends StatefulWidget{
 }
   
 class _FuelFormState extends State<FuelForm> {
-  
+  final double _formDistance = 5.0;
   final _currencies = ['Dollars', 'Euro', 'Pounds'];
   String _currency = 'Dollars';
   TextEditingController distanceController = TextEditingController();
+  TextEditingController avgController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
   String result = '';
   
   @override
@@ -43,7 +45,12 @@ class _FuelFormState extends State<FuelForm> {
         padding: EdgeInsets.all(15.0),
         child: Column(
           children: <Widget>[
-            TextField(
+            Padding(
+              padding: EdgeInsets.only(
+                top:_formDistance,
+                bottom:_formDistance
+              ),
+              child: TextField(
               controller: distanceController,
               decoration: InputDecoration(
                 labelText: 'Distance',
@@ -54,9 +61,46 @@ class _FuelFormState extends State<FuelForm> {
                   )
               ),
               keyboardType: TextInputType.number,
-              
+            )),
+            Padding(
+              padding: EdgeInsets.only(
+                top:_formDistance,
+                bottom:_formDistance
+              ),
+              child: TextField(
+              controller: avgController,
+              decoration: InputDecoration(
+                labelText: 'Distance per Unit',
+                hintText: 'e.g. 17',
+                labelStyle: textstyle,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0)
+                  )
+              ),
+              keyboardType: TextInputType.number,
+            )),
+            Padding(
+              padding: EdgeInsets.only(
+                top:_formDistance,
+                bottom:_formDistance
+              ),
+              child: 
+              Row(children: <Widget>[
+              Expanded(child: TextField(
+              controller: priceController,
+              decoration: InputDecoration(
+                labelText: 'Price',
+                hintText: 'e.g. 1.65',
+                labelStyle: textstyle,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0)
+                  )
+              ),
+              keyboardType: TextInputType.number,
+            ))],)
             ),
-            DropdownButton<String>(
+            Container(width: _formDistance *5),
+            Expanded(child: DropdownButton<String>(
               items: _currencies.map((String value){
                 return DropdownMenuItem<String>(
                   value: value,
@@ -66,14 +110,15 @@ class _FuelFormState extends State<FuelForm> {
               value: _currency,
               onChanged: (String value) {
                 _onDropdownChanged(value);
-              },
+              }),
               ), 
-              RaisedButton(
+              Row(children: <Widget>[
+                Expanded(child:RaisedButton(
                 color: Theme.of(context).primaryColorDark,
                 textColor: Theme.of(context).primaryColorLight,
                 onPressed: () {
                   setState(() {
-                    result = distanceController.text;
+                    result = _calculate();
                   });
                 },
                 child: Text(
@@ -81,7 +126,22 @@ class _FuelFormState extends State<FuelForm> {
                   textScaleFactor: 1.5,
                 ),
 
-              ),
+              )),
+              Expanded(child:RaisedButton(
+                color: Theme.of(context).buttonColor,
+                textColor: Theme.of(context).primaryColorDark,
+                onPressed: () {
+                  setState(() {
+                    _reset();
+                  });
+                },
+                child: Text(
+                  'Reset', 
+                  textScaleFactor: 1.5,
+                ),
+
+              )),
+              ]),
             Text(result)
           ]
         )
@@ -92,5 +152,24 @@ class _FuelFormState extends State<FuelForm> {
     setState((){
       this._currency = value;
     });
+  }
+
+String _calculate(){
+  double _distance = double.parse(distanceController.text);
+  double _consumption = double.parse(avgController.text);
+  double _fuelCost = double.parse(priceController.text);
+  double _totalCost = _distance / _consumption * _fuelCost;
+  String _result = 'The total cost of your trip is ' +
+    _totalCost.toStringAsFixed(2) + '' + _currency;
+  return _result;
+  }
+
+void _reset() {
+  distanceController.text = '';
+  avgController.text = '';
+  priceController.text = '';
+  setState(() {
+    result = '';
+  });
   }
 }
